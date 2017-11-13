c37f3c8cffe88b7a8ed3d48ac89b7cc4f8aacf86
romanvenica <romanvenica@gmail.com>
2017-11-13 01:18:01 -0300

    Traidos datos del servidor al hacer click en el punto del mapa

--- panelAdministrador/lite version/js/cargar-puntos.js
+++ panelAdministrador/lite version/js/cargar-puntos.js
@@ -1,7 +1,8 @@
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-	  // Especifica el lugar donde inicia el Google Maps, deberia ser la ubicacion del usuario.
-	  center: {lat: -34.789288, lng: -58.523531},
+	  // Especifica el lugar donde inicia el Google Maps, si el usuario acepta se va hasta su posicion.
+	  //center: {lat: -34.789288, lng: -58.523531},
+	  center: {lat: -34.8145869, lng: -58.4702204},
 	  zoom: 13,
 	  mapTypeControlOptions: {
 	    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
@@ -25,46 +26,99 @@ function initMap() {
 	    }
     */
 
-    // Array hardcodeado de puntos.	Tendria que traerlos de la BDD.
-	var locations = [	
-	{id:"loc1", lat: -34.789288, lng: -58.523531, radio: 15, tipo: "Banco", nombre:"El banco de carlitos"},
-	{id:"loc2", lat: -34.781, lng: -58.52359, radio: 15, tipo:"Banco", nombre:"Banco Ciudad"},
-	{id:"loc3", lat: -34.7899, lng: -58.52359, radio: 15, tipo:"Plaza", nombre:"Plaza 25 de mayo"}
-	]
+	// Este Ajax deberia llamar una funcion que solo traiga la cascara de los datos!
+	$.ajax({
+	  dataType: "json",
+	  url: "http://www.sucursal24.com/emanuel/apirest/cachengue/listar",
+	  data: null,
+	  success: function(result){
+	  	locations = result.data;
+	  	// Recorro la lista de puntines
+	  	locations.forEach(function(location){
+	  		// Seteo latitud y longitud en la variable point
+			var centro = new google.maps.LatLng(
+            location.posX, location.posY
+            );
+
+            // Da forma al circulo
+			var cityCircle = new google.maps.Circle({
+			strokeColor: '#FF0000',
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: '#FF0000',
+			fillOpacity: 0.35,
+			map: map,
+			center: centro,
+			// Hardcodeo el radio a 500 para que se vean en el mapa
+			// radius: location.radio
+			radius: 500
+			});
+
+		// Agrego al punto una accion.
+		cityCircle.addListener('click', function() {
+			idUnico = location.idCachengue
+			pedirDatosPuntos(idUnico);
+		});
+
+
+	  	});
+	  }
+	});
 
 	// Creo un array vacio que voy a llenar con las ubicaciones ya formateadas.
 	var markers = [];
 
 	// Crea ventana customizable de Google para setearsela a cada ubicacion.
 	var infoWindow = new google.maps.InfoWindow;
+}
 
-	// Funcion que recorre la lista de locaciones. Muy customizable.
-	locations.forEach(function(location){
-
-		// Seteo latitud y longitud en la variable point
-		var centro = new google.maps.LatLng(
-            location.lat, location.lng
-            );
-
-		var cityCircle = new google.maps.Circle({
-      	  strokeColor: '#FF0000',
-	      strokeOpacity: 0.8,
-	      strokeWeight: 2,
-	      fillColor: '#FF0000',
-	      fillOpacity: 0.35,
-	      map: map,
-	      center: centro,
-	      radius: location.radio
-	    });
-
-		// Agrego al punto una accion.
-		cityCircle.addListener('click', function() {
-			console.log("test");
-		});
-
+function pedirDatosPuntos(idUnico){
+	$.ajax({
+	  dataType: "json",
+	  // Esto deberia ser una funcion que tome parametro ID y devuelva el resultado. Lo hago asi mientras tanto.
+	  url: "http://www.sucursal24.com/emanuel/apirest/cachengue/listar/",
+	  data: null,
+	  success: function(result){
+	  	datosLocacion = result.data
+	  	datosLocacion.forEach(function(location){
+	  		if(location.idCachengue == idUnico){
+	  			completarDatos(location);
+	  		}
+	  	});	
+	  }
 	});
+}
+
+function completarDatos(location){
+	$("#botonCargarModificar").html("Modificar");
+	$("#botonCargarModificar").removeClass("btn-danger");
+	$("#botonCargarModificar").addClass("btn-warning");
+
+	$("#checkboxActiva").prop('checked', location.activa);
+	$("#inputNombre").val(location.nombre);
+	$("#selectTipo").val(location.tipo);
+
+	// Faltan los dias de semana. Quedan hardcodeados hasta que este.
+	$("#checkboxLunes").prop('checked', true);
+	$("#checkboxMartes").prop('checked', true);
+	$("#checkboxMiercoles").prop('checked', true);
+	$("#checkboxJueves").prop('checked', true);
+	$("#checkboxViernes").prop('checked', true);
+
+	var horaInicio = location.horaIncio.slice(0,2)+":"+location.horaIncio.slice(2,4);
+	$("#inputHoraInicio").val(horaInicio);
+
+	var horaFin = location.horaFin.slice(0,2)+":"+location.horaFin.slice(2,4);
+	$("#inputHoraFin").val(horaFin);
+	
+	$("#inputCajaComentario").val(location.comentario);
+
+	$("#inputCantMinima").val(location.usuariosMinimos);
 
 }
+
+
+		// Esto funcionaba si los puntos eran puntos y no areas. Si son areas no anda.
 
 		/*
 
