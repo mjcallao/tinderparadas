2a4852632aedb547e3b9687819a4d947392deaef
JonathanEmanuel <cruzjonathan.e@gmail.com>
2017-11-20 13:21:08 -0300

    Correcion de errores de sintaxis

--- apiRest/modulos/controllers/cachengueController.php
+++ apiRest/modulos/controllers/cachengueController.php
@@ -98,18 +98,18 @@
 			// idCachengue, nombre, posX, posY, radio, activa, tipo, comentario, diasActivo, horaIncio, horaFin, usuariosMinimos, usuariosActivos
 			if(isset($_POST['nombre']) && isset($_POST['posX']) && isset($_POST['posY']) && isset($_POST['radio']) && isset($_POST['activa']) && isset($_POST['tipo']) && isset($_POST['comentario']) && isset($_POST['diasActivo']) && isset($_POST['horaIncio']) && isset($_POST['horaFin']) && isset($_POST['usuariosMinimos']) && isset($_POST['usuariosActivos'])) {
 
-				$array = array('nombre' => , $_POST['nombre']),
-								'posX' => , $_POST['posX'])
-								'posY' => , $_POST['posY'])
-								'radio' => , $_POST['radio'])
-								'activa' => , $_POST['activa']),
-								'tipo' => , $_POST['tipo'])
-								'comentario' => , $_POST['comentario'])
-								'diasActivo' => , $_POST['diasActivo'])
-								'horaIncio' => , $_POST['horaIncio']),
-								'horaFin' => , $_POST['horaFin'])
-								'usuariosMinimos' => , $_POST['usuariosMinimos'])
-								'usuariosActivos' => , $_POST['usuariosActivos'])
+				$array = array('nombre' => $_POST['nombre'],
+								'posX' => $_POST['posX'],
+								'posY' => $_POST['posY'],
+								'radio' => $_POST['radio'],
+								'activa' => $_POST['activa'],
+								'tipo' => $_POST['tipo'],
+								'comentario' => $_POST['comentario'],
+								'diasActivo' => $_POST['diasActivo'],
+								'horaIncio' => $_POST['horaIncio'],
+								'horaFin' => $_POST['horaFin'],
+								'usuariosMinimos' => $_POST['usuariosMinimos'],
+								'usuariosActivos' => $_POST['usuariosActivos']
 								);
 				return $array;
 
