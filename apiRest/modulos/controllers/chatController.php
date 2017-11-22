7d32479869d9073f73eedc3579348ca1b84036bd
JonathanEmanuel <cruzjonathan.e@gmail.com>
2017-11-22 01:34:48 -0300

    Modulo chat  + leer/id  * guardar

--- apiRest/modulos/controllers/chatController.php
+++ apiRest/modulos/controllers/chatController.php
@@ -3,13 +3,14 @@
 	header("Access-Control-Allow-Origin: *");
 	// Permite la ejecucion de los metodos
 	header("Access-Control-Allow-Methods: GET, POST");
+	header('Content-type: application/json; charset=utf-8');
 
 	require_once(MODELS . 'chatModel.php');
 	require_once(VIEWS . 'chatView.php');
 
 
 	/**
-	 * 
+	 * leer/$id : retorna los chat por el id del cachengue
 	*/
 class chatController {
 		
@@ -19,27 +20,36 @@ class chatController {
 			$chatInst = new Chat();
 			$chatInst->get(1);
 			
-			
+			echo json_encode($chatInst->mensajes);
 
-			$instanciaView = new VistaChat($chatInst->mensajes);
-			$instanciaView->mostrar();
+			//$instanciaView = new VistaChat($chatInst->mensajes);
+			//$instanciaView->mostrar();
 		}
 
 	}
 
 	public function guardar($datos = '') {
 		
-		//$mensaje  = array('nick' => 'user', 'texto' => 'bla bla bla');
-	
+		//$mensaje  = array('nick' => 'user', 'mensaje' => 'bla bla bla', 'cachengue' => id);
+		$postData = file_get_contents("php://input");
+		if (isset($postData)) {
+			$mensaje = array($_POST['nick'], $_POST['mensaje'], $_POST['idCachengue'] );
+			//echo json_encode($mensaje);
+			$chatInst =  new Chat();
+			$chatInst->set($mensaje);
 
-		$mensajeValido = $this->validarDatoPOST(); // recibe los datos ya validados
+		}
 
+		//$mensajeValido = $this->validarDatoPOST(); // recibe los datos ya validados
+		/*
+		print_r($mensajeValido);
 		if(is_array($mensajeValido)) {
 			
 			$chatInst =  new Chat();
 			// es array ('nick' => 'user', 'texto' => 'bla bla bla')
 			$chatInst->set($mensajeValido);
 		}
+		*/
 		
 	}
 
@@ -50,12 +60,15 @@ class chatController {
 		if (isset($_POST['nick']) && isset($_POST['mensaje'])) {
 			$nick = $_POST['nick'];
 			$mensaje = $_POST['mensaje'];
-			return array($nick, $mensaje);
+			$cachengue = 1;  // el ID del cachengue
+			return array($nick, $mensaje, $cachengue);
 		} else {
 			return '';
 		}
 
 	}
+
+
 }
 
 ?>
